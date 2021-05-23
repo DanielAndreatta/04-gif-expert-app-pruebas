876635b9a4e17ace0bd33fe9a82c40f84f4ae30b
Daniel <danielandreatta2@gmail.com>
2021-05-22 23:24:17 -0300

    push

---
+++ src/components/GifGridItem.js
@@ -0,0 +1,25 @@
+
+
+import React from 'react'
+
+import PropTypes from 'prop-types';
+
+
+export const GifGridItem = ( { title, url } ) => {
+    
+   // console.log( { id, title, url} );
+
+    return (
+        <div className="card animate__animated animate__bounce">
+            <img src={url} alt={title} />
+            <p> { title } </p>
+        </div>
+    )
+}
+
+GifGridItem.propTypes = {
+
+    title: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired
+
+}  
